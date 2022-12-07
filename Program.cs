3dae1aef1ac1d91a29d115af97bcac50ba5a57ce
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2022-12-07 09:28:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemIOFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite Vaše ime: ");
+            string ime = Console.ReadLine();
+
+            string datoteka = "ime.txt";
+
+            if(File.Exists(datoteka))
+            {
+                if(!Directory.Exists("backup"))
+                {
+                    Directory.CreateDirectory("backup");
+                }
+                File.Copy(datoteka, "backup\\ime_"
+                    + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")
+                    + ".txt");
+                File.Delete(datoteka);
+            }
+            File.WriteAllText(datoteka, ime);
+            Console.ReadKey();
+        }
+    }
+}
